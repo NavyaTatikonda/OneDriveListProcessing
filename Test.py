ad55994953abc1cede7c4264865d6681db182fd4
NavyaTatikonda <inavyatatikonda@gmail.com>
2020-07-28 18:14:33 +0530

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df_msol = pd.read_csv("MSOLusersReport.csv")
+df_eur = pd.read_csv("EURReport.csv")
+print(df_msol.head(5))
+print(df_eur.head(5))
+
+df_merged = pd.merge(df_eur, df_msol, how="left", on="RelativePath_u")
+
+filtered_df = df_merged[(df_merged['BlockCredential'].isnull())|(df_merged['BlockCredential'] == True)]
+filtered_df = filtered_df[(filtered_df['SharingCapability'] != "Disabled")]
+filtered_df = filtered_df[(filtered_df['LockState'] == "Unlock")]
+
+print(filtered_df.info)
